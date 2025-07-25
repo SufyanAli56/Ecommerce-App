49da0fbb16794b56daa3fa97c0615def8b000909
SufyanAli56 <sufyanalii1127@gmail.com>
2025-07-25 10:37:30 -0700

    modified:   clients/Frontend/src/components/navbar/Navbar.jsx

--- clients/Frontend/src/components/navbar/Navbar.jsx
+++ clients/Frontend/src/components/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { FaHeart, FaShoppingCart } from 'react-icons/fa';
+import React from "react";
+import { FaHeart, FaShoppingCart } from "react-icons/fa";
 
 const MainNavbar = () => {
   return (
     <nav className="bg-white px-6 py-4 flex justify-between items-center shadow-sm font-poppins">
       <div className="text-3xl font-bold text-gray-800 flex items-center">
-        <span className="text-pink-600 text-4xl mr-1">E</span>Shopper
+        <span className="bg-white border border-gray-300 text-pink-600 text-3xl font-bold px-3 py-1  mr-2 shadow-sm">
+          E
+        </span>
+        Shopper
       </div>
       <input
         type="text"
@@ -15,8 +18,6 @@ const MainNavbar = () => {
       <div className="flex items-center space-x-6">
         <FaHeart className="text-xl cursor-pointer hover:text-pink-500" />
         <FaShoppingCart className="text-xl cursor-pointer hover:text-blue-600" />
-        <a href="#" className="hover:underline">Login</a>
-        <a href="#" className="hover:underline">Register</a>
       </div>
     </nav>
   );
